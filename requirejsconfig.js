5bd07f33adfb9cad666ee9145085f26d0ef493b6
Mihael Konjevic <konjevic@gmail.com>
2014-07-22 10:18:57 +0200

    Add scenario player

--- requirejsconfig.js
+++ requirejsconfig.js
@@ -5,6 +5,12 @@ requirejs.config({
     mustache : 'bower_components/require-can-renderers/lib/mustache',
     stache   : 'bower_components/require-can-renderers/lib/stache',
     ejs      : 'bower_components/require-can-renderers/lib/ejs',
-    lodash   : 'bower_components/lodash/dist/lodash'
+    lodash   : 'bower_components/lodash/dist/lodash',
+    cucumber : 'bower_components/cucumber-js/release/cucumber',
+  },
+  shim : {
+  	cucumber : {
+  		exports : 'Cucumber'
+  	}
   }
 });
